21178774880faa730ae0bcabc415ed19c63caa9f
viettrungIT3 <viettrungcntt03@gmail.com>
2026-03-04 20:17:16 +0700

    feat(api): add shared config and logger skeleton

---
+++ src/shared/logger/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const pino = require('pino');
+
+module.exports = pino({
+  level: process.env.LOG_LEVEL || 'info',
+  transport: process.env.NODE_ENV !== 'production' ? {
+    target: 'pino-pretty',
+    options: { colorize: true }
+  } : undefined
+});
